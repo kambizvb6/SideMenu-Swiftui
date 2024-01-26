d154b6c1de31af0168a7157be4335847ea6ca197
Masoud Roosta <31803762+MasoudRoosta@users.noreply.github.com>
2024-01-26 07:20:27 +0100

    Update ContentView.swift

--- SideMenu/ContentView.swift
+++ SideMenu/ContentView.swift
@@ -17,25 +17,17 @@ struct ContentView: View {
             ZStack {
                 TabView(selection: $selectedTab){
                     ForEach(SideMenuOptionModel.allCases) { option in
-                        switch option {
-                        case .home:
-                            Text("Home")
-                                .tag(option.rawValue)
-                        case .visitors:
-                            Text("Visitors")
-                                .tag(option.rawValue)
-                        case .summary:
-                            Text("Summary")
-                                .tag(option.rawValue)
-                        case .searchEngin:
-                            Text("Search Engin")
-                                .tag(option.rawValue)
-
-                        }
+                        //Here we don't need to use switch
+                        Text(option.title)
+                            .tag(option.rawValue)
                     }
                 }
                 
                 SideMenuView(isShowing: $showMenu, selectedOption: $selectedOption)
+                // A simple transition and animation make it more attractive 
+                    .transition(.move(edge: .leading))
+                    .animation(.spring(), value: showMenu)
+                
             
             }
             .toolbar(showMenu ? .hidden : .visible, for: .navigationBar)
